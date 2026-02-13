33f62fc2ef9b2f7458c2388e9026b7618be0b034
25f3001999-hue <25f3001999@ds.study.iitm.ac.in>
2026-02-13 19:20:16 +0000

    done

--- src/12-season-activity.js
+++ src/12-season-activity.js
@@ -32,4 +32,48 @@
  */
 export function getSeasonActivity(month, temperature) {
   // Your code here
+  let Object= {season:"" , activity:""}
+  if (month<1 || month>12) {
+    return null;
+  }
+  else if (month===1 || month===2 || month===12) {
+     Object.season= "Winter"
+     if (temperature<0) {
+      Object.activity="skiing"
+     }
+     else if (temperature>=0) {
+      Object.activity="ice skating"
+      
+     }
+  }
+   else if (month===4 || month===3 || month===5) {
+     Object.season= "Spring"
+     if (temperature>20) {
+      Object.activity="hiking"
+     }
+     else if (temperature<=20) {
+      Object.activity="museum visit"
+     }
+    
+  }
+   else if (month===6 || month===7 || month===8) {
+    Object.season= "Summer"
+    if (temperature>35) {
+      Object.activity="swimming"
+     }
+    else  if (temperature<=35) {
+      Object.activity="cycling"
+     }
+  }
+  else if (month===9 || month===10 || month ===11) {
+   Object.season= "Autumn"
+    if (temperature>15) {
+      Object.activity="nature walk"
+     }
+     else if (temperature<=15) {
+      Object.activity="reading at a cafe"
+     }
+  }
+ 
+return Object
 }
